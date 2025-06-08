45d9145f517ff383b6cb5d329fe92c82e610bff4
questcoderull <questcoderull@gmail.com>
2025-06-08 13:19:14 +0600

    pacth a user field and update user lastSignInTime after signIn added

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { use } from "react";
+import { AuthContext } from "../Context/AuthContex";
 
 const SignIn = () => {
+  const { signInUser } = use(AuthContext);
+
   const handleSignIn = (e) => {
     e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+
+    // firebase sign in send
+    signInUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+
+        const signInInfo = {
+          email,
+          lastSignInTime: result.user?.metadata?.lastSignInTime,
+        };
+        //update last sign in to the database.
+        fetch("http://localhost:5000/users", {
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(signInInfo),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("after update patch", data);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div className="card bg-base-100 max-w-sm mx-auto shrink-0 shadow-2xl border border-red-600">
@@ -24,7 +57,9 @@ const SignIn = () => {
             className="input"
             placeholder="Password"
           />
-          <div></div>
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
           <button className="btn btn-neutral mt-4">Sign In</button>
         </form>
       </div>
