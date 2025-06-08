3d89a2fb8faf7602a2ad5970118bdf05ef08d195
questcoderull <questcoderull@gmail.com>
2025-06-08 14:42:21 +0600

    firebase deploy added

---
+++ src/_client_side_firebase_deploy.js
@@ -0,0 +1,24 @@
+/**
+ * 1.inside firebase project (console.firebase.google.com) start hosting.
+ * 
+ * 
+ * one time for your computer 
+ * 2.npm install -g firebase-tools
+ * 3.firebase login
+ * 
+ * for each project 
+ * 4.firebase init
+ * 5.do you want to proceed? ; y
+ * 6.Hosting select
+ * 7.existting project (select using up and down arrow)
+ * 8. what is your public directory? ans: dist.
+ * 9.single page rewrite. y.
+ * 10.Set up automatic builds and deploys with  
+GitHub? : n
+
+
+
+*For everytime you want to deploy.
+* 1.npm run build
+*2.firebase deploy
+ */
--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
         Component: Home,
       },
       {
-        path: "/addCoffe",
+        path: "/addCoffee",
         Component: AddCoffee,
       },
       {
